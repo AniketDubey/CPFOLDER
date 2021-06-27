a9753b331723b2c57f5d6c1de74fc082915758ab
AniketDubey <aniket.dubey0206@gmail.com>
2021-06-27 19:26:55 +0530

    CPFOLDER

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -1,48 +1,321 @@
-
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <bits/stdc++.h>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <string.h>
+#include <map>
+#include <queue>
+#include <cmath>
 using namespace std;
 typedef long long int ll;
-#define IOS                           \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
+typedef long double ld;
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
-#define mod 1000000007
+string en="\n";
+string sp=" ";
 
-#define w(x)  \
-    int x;    \
-    cin >> x; \
-    while (x--)
+/*******************************************************************************************/
+
+#define mod 								1000000007
+#define INF 								LLONG_MAX
+#define NINF								LLONG_MIN
+#define PII 								3.141592653589793238462643383279502884197169399375
 
 
-ll modularexpo(ll x, ll y, ll p)
+#define vp(vi,x) 							cin>>x; 		vi.pb(x);		
+#define bsearch(a,x)						binary_search(all(a),x)
+#define LB(a,x) 							(std::lower_bound(all(a),x)-a.B)
+#define UB(a,x) 							(std::upper_bound(all(a),x)-a.B)
+#define smallest(a) 						*min_element(a.begin(),a.end())
+#define largest(a) 							*max_element(a.begin(),a.end())
+#define all(a) 								a.begin(), a.end()
+#define sortdesc(a) 						sort(a.begin(),a.end(),greater<ll>())
+#define ps(x, y) 							fixed << setprecision(y) << x
+#define sz(a) 								(ll)a.size()
+#define E 									end()
+#define B 									begin()
+
+/*******************************************************************************************/
+
+#define yes 								cout<<"YES\n"
+#define no 									cout<<"NO\n"
+#define gcd 								__gcd
+#define lcm(a,b)							((a*b)/__gcd(a,b))
+#define ff 									first
+#define ss 									second
+#define pb 									push_back
+#define mp 									make_pair
+#define pii 								pair<ll, ll>
+#define vi 									vector<ll>
+#define mii 								map<ll,ll>
+#define mci									map<char,ll>
+#define msi									map<string,ll>
+#define pqb 								priority_queue<ll>
+#define pqs 								priority_queue<ll, vi, greater<ll>>
+
+/*******************************************************************************************/
+#define w(x)  								int x;    cin >> x; 	while (x--)
+#define for0(i, n) 							for (ll i = 0; i < n; i++)
+#define for1(i, n) 							for (ll i = 1; i <= n; i++)
+#define loop(i, a, b) 						for (ll i = a; i < b; i++)
+#define bloop(i, a, b) 						for (ll i = a; i >= b; i--)			
+#define trav(g,arr)							for(auto g: arr) {cout<<g<<sp;} cout<<endl;
+/*******************************************************************************************/
+
+/*vi factor;
+void fact(ll n)
 {
-    ll res = 1;
-    x = x % p;
-    if (x == 0)
-        return 0;
-    while (y > 0)
+
+    for(ll i=1;i<=sqrt(n);i++)
     {
-        if (y & 1)
-            res = ((res % p) * (x % p)) % p;
-        y = y >> 1;
-        x = ((x % p) * (x % p)) % p;
+        if(n%i==0)
+        {
+            if(n/i==i)
+                factor.pb(i);
+            else
+            {
+                factor.pb(i);
+                factor.pb(n/i);
+            }
+        }
+    }
+
+    sort(all(factor));
+}*/
+
+/*******************************************************************************************/
+
+/*vi primenum;
+void findprime(ll n)
+{
+    bool p[n+1];
+    memset(p, true, sizeof(p));
+
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (p[i] == true)
+        {
+            for (ll j = i * i; j <= n; j += i)
+                p[j] = false;
+        }
+    }
+
+    for (ll i = 2; i <= n; i++)
+    {
+        if (p[i] == true)
+            primenum.pb(i);
+    }
+
+}*/
+
+/*******************************************************************************************/
+
+/*ll lcmofarray(vi arr)
+{
+    ll n = sz(arr);
+    ll ans=1;
+    for(ll i=0;i<n;i++)
+    {
+        ans = (arr[i]*ans)/(__gcd(arr[i],ans));
+    }
+    return ans;
+
+}*/
+
+/*******************************************************************************************/
+
+/*ll gcdofarray(vi arr)
+{
+    ll n = sz(arr);
+     ll ans=arr[0];
+     for(ll i=1;i<n;i++)
+            ans = __gcd(ans,arr[i]);
+     return ans;
+}*/
+
+/*******************************************************************************************/
+
+/*ll findgcd(ll a, ll b)
+{
+
+    if (b == 0)
+        return a;
+    return findgcd(b, a % b);
+
+}*/
+
+/*******************************************************************************************/
+
+/*ll expomod(ll x,ll y,ll p)
+{
+    ll res=1;
+    x = x%p;
+    if(x==0)
+        return 0;
+    while(y>0)
+    {
+        if(y&1)
+            res = ((res%p)*(x%p))%p;
+        y=y>>1;
+        x = ((x%p)*(x%p))%p;
     }
     return res;
-}
+}*/
 
+/*******************************************************************************************/
+
+/*ll ar[1000001];
+void calc()
+{
+    ll maxn = 1000000;
+    for (ll i = 1; i <= maxn; i++)
+        ar[i] = -1;
+        
+    ar[1] = 1;
+    for (ll i = 2; i <= maxn; i++)
+    {
+        if (ar[i] == -1)
+        {
+            for (ll j = i; j <= maxn; j = j + i)
+            {
+                if (ar[j] == -1)
+                    ar[j] = i;
+            }
+        }
+    }
+}*/
+
+/*******************************************************************************************/
+
+/*ll phi(ll n) 
+{ 
+    ll result = n;  
+   
+    for(ll p = 2; p * p <= n; ++p) 
+    { 
+          
+        if (n % p == 0)  
+        { 
+              
+            while (n % p == 0) 
+                n /= p; 
+                  
+            result -= result / p; 
+        } 
+    } 
+   
+    if (n > 1) 
+        result -= result / n; 
+          
+    return result; 
+}*/
+
+/*******************************************************************************************/
+
+/*ll Pow(ll a, ll x)
+{
+    if (x == 0)
+        return 1;
+    else
+    {
+        ll y = a;
+        for (ll i = 1; i < x; i++)
+        {
+            a = a * y;
+        }
+        return a;
+    }
+}*/
+
+/*******************************************************************************************/
 
 int main()
 {
     IOS;
-
+  
     w(t)
     {
-        ll n;
-        cin >> n;
+        ll n,m,k;
+        cin>>n>>m>>k;
 
-        cout << modularexpo(2, n - 1, mod) << "\n";
+        vector<pair<ll, ll>> vec;
+
+        for0(i,n*m)
+        {
+            ll x,y;
+            cin>>x>>y;
+            vec.pb(mp(x, y));
+        }
+
+        ll low = 0, high = (n * m) - 1, ans = (n*m);
+
+        // if mid  = 5 and at that movement there is no winner so that means that there will not be any winner
+        // before chance no. 5 and hence increase low = mid+1 as winner would be possible only after mid and
+        // before high.............
+        // Similarly if at mid = 10 we have a winner so decrease high = mid-1, and check from low to mid again
+
+
+
+        while(low<=high)
+        {
+            ll mid = (low + high) / 2;
+            ll arr[n + 1][m + 1] = {0};
+
+
+            // populate the matrix upto mid chances..........
+            for0(i,mid+1)
+            {
+                if(i%2==0)
+                    arr[vec[i].ff][vec[i].ss] = 1;
+                else
+                    arr[vec[i].ff][vec[i].ss] = -1;
+            }
+
+            // calculate prefix sum of the matrix formed upto mid chances...........
+            //  here we have created a 2d prefix sum matrix 
+            for1(i,n)
+            {
+                for1(j,m)
+                {
+                    // formula for 2d prefix sum.........
+                    // prefix[i][j] = arr[i][j] + arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1]
+                    arr[i][j] = arr[i][j] + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+                }
+            }
+
+            bool found = false;
+
+            for (ll i = k; i <= n;i++)
+            {
+                for (ll j = k; j <= m;j++)
+                {
+                    ll cnt = arr[i][j] - arr[i - k][j] - arr[i][j - k] + arr[i - k][j - k];
+
+                    if(abs(cnt)==(k*k))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if(found==true)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+
+        if(ans==(n*m))
+            cout << "Draw\n";
+        else if(ans%2==0)
+            cout << "Alice\n";
+        else
+            cout << "Bob\n";
     }
 
     return 0;
